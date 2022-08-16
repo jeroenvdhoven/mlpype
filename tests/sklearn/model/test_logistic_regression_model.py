0a5ae1a1de1a443852cd9404355d42dd7979d662
Jeroen van den Hoven <jeroen.vandenhoven@adidas.com>
2022-08-16 16:59:10 +0200

    Add tests for linear and logistic models

---
+++ tests/sklearn/model/test_logistic_regression_model.py
@@ -0,0 +1,18 @@
+from unittest.mock import MagicMock
+
+from sklearn.linear_model import LogisticRegression
+
+from pype.sklearn.model.logistic_regression_model import LogisticRegressionModel
+
+
+class Test_LogisticRegressionModel:
+    def test_init_new_model(self):
+        model = LogisticRegressionModel(["x"], ["y"])
+
+        assert isinstance(model.model, LogisticRegression)
+
+    def test_init_existing_model(self):
+        sklearn_model = MagicMock()
+        model = LogisticRegressionModel(["x"], ["y"], model=sklearn_model)
+
+        assert model.model == sklearn_model
