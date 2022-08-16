95d00865a6f32ea297fce9ed9d5ff0bb03f4f9f2
Jeroen van den Hoven <jeroen.vandenhoven@adidas.com>
2022-08-16 16:22:47 +0200

    Add serialiser tests

---
+++ tests/base/serialiser/test_joblib_serialiser.py
@@ -0,0 +1,23 @@
+from unittest.mock import patch
+
+from pype.base.serialiser.joblib_serialiser import JoblibSerialiser
+
+
+class Test_JoblibSerialiser:
+    def test_save(self):
+        with patch("pype.base.serialiser.joblib_serialiser.dump") as mock_dump:
+            serialiser = JoblibSerialiser()
+            obj = [None]
+            file = "some file"
+            serialiser.serialise(obj, file)
+
+            mock_dump.assert_called_once_with(obj, file)
+
+    def test_load(self):
+        with patch("pype.base.serialiser.joblib_serialiser.load") as mock_load:
+            serialiser = JoblibSerialiser()
+            file = "some file"
+            result = serialiser.deserialise(file)
+
+            mock_load.assert_called_once_with(file)
+            assert mock_load.return_value == result
