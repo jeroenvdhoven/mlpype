bdd9c243d8ce4288b6cae3e014e873d144ac60a4
Jeroen van den Hoven <jeroen.vandenhoven@adidas.com>
2022-09-29 16:21:43 +0200

    Add PypeApp to wheel deployments

--- pype/base/deploy/wheel_helpers/wheel_main.py
+++ pype/base/deploy/wheel_helpers/wheel_main.py
@@ -1,6 +1,8 @@
 from pathlib import Path
 
-from pype.base.deploy import Inferencer
+from fastapi import FastAPI
+
+from pype.base.deploy import Inferencer, PypeApp
 
 
 def load_model() -> Inferencer:
@@ -12,3 +14,24 @@ def load_model() -> Inferencer:
     folder = Path(__file__).parent / "outputs"
     print(f"Loading inferencer from {str(folder)}")
     return Inferencer.from_folder(folder)
+
+
+def load_app() -> FastAPI:
+    """Wrapper function to create a FastAPI server from a fixed location in this package.
+
+    Based on PypeApp.
+
+    If you create a python file F where this App is loaded:
+    ```
+    from <model name> import load_app
+    app = load_app()
+    ```
+
+    You can host this model using a command like:
+    uvicorn <python package path to file F>:app
+
+    Returns:
+        FastAPI: The FastAPI/PypeApp server used to host the model.
+    """
+    folder = Path(__file__).parent / "outputs"
+    return PypeApp("model", folder=folder).create_app()
