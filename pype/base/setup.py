d817bbab43d89f58293c240f03afddac0db0a99e
Jeroen van den Hoven <jeroen.vandenhoven@adidas.com>
2022-10-06 17:07:36 +0200

    Shuffle dependencies for base package

--- pype/base/setup.py
+++ pype/base/setup.py
@@ -1,8 +1,8 @@
 from setuptools import find_namespace_packages, setup
 
-dev_deps = ["pre-commit", "pytest-cov"]
-test_deps = ["pytest"]
-deps: list[str] = ["docstring_parser==0.14.1", "pydantic==1.9.1", "joblib==1.1.0", "uvicorn==0.18.2", "fastapi==0.79.0"]
+dev_deps = ["pre-commit", "uvicorn==0.18.2"]
+test_deps = ["pytest", "pytest-cov"]
+deps: list[str] = ["docstring_parser==0.14.1", "pydantic==1.9.1", "joblib==1.1.0", "fastapi==0.79.0"]
 
 setup(
     name="pype-base",
