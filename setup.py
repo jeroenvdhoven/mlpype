4bdaa2e373b25cd25244dc5d4cdab99efbcf2924
Jeroen van den Hoven <jeroen.vandenhoven@adidas.com>
2023-01-06 13:46:06 +0100

    Add tag-publish cicd pipeline on tags, update package version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     packages=find_namespace_packages(include=["pype.*"]),
     namespace_packages=["pype"],
     python_requires=">=3.8",
-    version="0.1.1",
+    version="0.1.2",
     license="MIT",
     author="Jeroen van den Hoven",
     url="https://github.com/jeroenvdhoven/pype",
