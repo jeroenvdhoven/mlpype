3e5abdb1c18565fa1b542e97bbc2a70d1c01ed21
Jeroen van den Hoven <jeroen.vandenhoven@adidas.com>
2023-01-20 14:53:46 +0100

    Add sql source in sklearn data

---
+++ tests/sklearn/data/test_sql_source.py
@@ -0,0 +1,17 @@
+from unittest.mock import patch
+
+from pype.sklearn.data import SqlSource
+
+
+class TestSqlSource:
+    def test_read(self):
+        query = "Select * from *"
+        conn = "sqlite://nowhere"
+        extra = [4, 5]
+        source = SqlSource(query, conn, 1, 2, extra=extra)
+
+        with patch("pype.sklearn.data.sql_source.pd.read_sql") as mock_read_sql:
+            result = source.read()
+
+        mock_read_sql.assert_called_once_with(1, 2, sql=query, con=conn, extra=extra)
+        assert result == mock_read_sql.return_value
