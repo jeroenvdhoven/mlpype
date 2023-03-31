bcf6cf2fee23bec8c0c66c8280c018c9b2b2f2c8
Jeroen van den Hoven <jeroen.vandenhoven@adidas.com>
2023-03-31 16:31:48 +0200

    First attempt at adding documentation in

--- pype/spark/setup.py
+++ pype/spark/setup.py
@@ -1,20 +1,21 @@
 from setuptools import find_namespace_packages, setup
 
-version = "0.2.0"
+if __name__ == "__main__":
+    version = "0.2.0"
 
-deps = [
-    f"pype-base=={version}",
-    # We will provide absolute no guarantees that our integration will work with
-    # EVERY version of pyspark. This has been developed under pyspark==3.2.1.
-    "pyspark>=3.2.1",
-]
-strict_deps = [s.replace(">=", "==") for s in deps]
-test_deps = ["pandas"]
+    deps = [
+        f"pype-base=={version}",
+        # We will provide absolute no guarantees that our integration will work with
+        # EVERY version of pyspark. This has been developed under pyspark==3.2.1.
+        "pyspark>=3.2.1",
+    ]
+    strict_deps = [s.replace(">=", "==") for s in deps]
+    test_deps = ["pandas"]
 
-setup(
-    name="pype-spark",
-    install_requires=deps,
-    extras_require={"dev": strict_deps + test_deps, "test": strict_deps + test_deps, "strict": strict_deps},
-    packages=find_namespace_packages(include=["pype.*"]),
-    version=version,
-)
+    setup(
+        name="pype-spark",
+        install_requires=deps,
+        extras_require={"dev": strict_deps + test_deps, "test": strict_deps + test_deps, "strict": strict_deps},
+        packages=find_namespace_packages(include=["pype.*"]),
+        version=version,
+    )
