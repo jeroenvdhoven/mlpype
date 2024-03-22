5ec7a5b122fb0258f362c534d3a4b812846bace7
Jeroen van den Hoven <jeroenhoven@gmail.com>
2024-03-22 09:24:41 +0100

    Changes to build script name and IP parsing in installation

--- scripts/dev_install.sh
+++ scripts/dev_install.sh
@@ -10,9 +10,9 @@ function install_package () {
     if [ $2 != "local" ];
     then
         echo "Installing $1 from host $2"
-        ip=`echo $2 or grep -oE [0-9]+\.[0-9]+\.[0-9]+\.[0-9]+`
+        ip=`echo $2 | grep -oE [0-9]+\.[0-9]+\.[0-9]+\.[0-9]+`
         echo "IP: $ip"
-        pip install -i $2 $1 --trusted-host $ip --upgrade
+        pip install --index-url $2 $1 --trusted-host $ip --upgrade
     else
         # make sure we use the right path for local installation
         if [ $1 == "mlpype" ];
