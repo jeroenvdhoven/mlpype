8cb9364b5b6a62990f90569919bf9caf37867737
Jeroen.vandenHoven <jeroen.vandenhoven@eneco.com>
2024-05-17 17:01:30 +0200

    Update pandas and sklearn due to 3.11 and 3.12 issues

--- mlpype-tensorflow/setup.py
+++ mlpype-tensorflow/setup.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
         f"mlpype-base=={version}",
         # on mac, it is recommended to use conda/mamba or source to install tensorflow
         "tensorflow>=2.12.1",
-        "numpy>=1.23.0",
+        "numpy>=1.23.2",
         "protobuf>=3.20.3",
     ]
     strict_deps = [s.replace(">=", "==") for s in deps]
