cd783db6407c6b739e2a67cb3c685a9cbf82b5c8
Jeroen.vandenHoven <jeroen.vandenhoven@eneco.com>
2024-05-27 17:45:13 +0200

    Drop support for 3.8

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_namespace_packages, setup
 setup(
     name="mlpype",
     packages=find_namespace_packages(include=["mlpype.*"]),
-    python_requires=">=3.8",
+    python_requires=">=3.9",
     version="0.5.2",
     license="MIT",
     author="Jeroen van den Hoven",
