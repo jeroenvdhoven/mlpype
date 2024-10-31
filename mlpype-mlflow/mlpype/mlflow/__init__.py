709522e4a3d4e658dd7f9cd818be442f759a27c2
Jeroen.vandenHoven <jeroen.vandenhoven@eneco.com>
2024-10-31 08:52:59 +0100

    Attempt to fix CI

--- mlpype-mlflow/mlpype/mlflow/__init__.py
+++ mlpype-mlflow/mlpype/mlflow/__init__.py
@@ -1,9 +1,14 @@
 """Provides tools for using MLpype with MLflow.
 
-At the moment, MLflow is the recommended logging tool for MLpype. This package provides 2 main tools:
+At the moment, MLflow is the recommended logging tool for MLpype. This package provides a few tools:
 
 - An MlflowLogger: An MLpype logger that uses MLflow. Set this up and you don't have to worry about how to\
     log your model.
-- A quick function to load an Inferencer from an experiment stored in MLflow.
+- A quick function to load an Inferencer from an experiment stored in MLflow. You can find this in
+    `mlpype.mlflow.deploy`.
+- A mlflow compatible version of a Model that should work with any MLpype model. It extends mlflow's PythonModel.
+    This should allow you to load any model that has been stored in MLflow using mlflow's logic. You don't
+    need to do anything to use this except use the MLflowLogger. Once registered, the model can loaded using
+    mlflow's `from mlflow.pyfunc import load_model`.
 """
 from . import deploy, logger
--- mlpype-sklearn/mlpype/sklearn/__init__.py
+++ mlpype-sklearn/mlpype/sklearn/__init__.py
@@ -1,10 +1,12 @@
 """Provides tools for using Sklearn with MLpype.
 
-This provides 3 main tools:
+This provides a few tools:
 
-- An SklearnModel: An MLpype model that uses Sklearn. This is easily extended to most models in Sklearn.
-- Type checkers for Sklearn data
+1. An SklearnModel: An MLpype model that uses Sklearn. This is easily extended to most models in Sklearn.
+This can be found in `mlpype.sklearn.model`. This module also contains pre-loaded models from Sklearn.
+2. Type checkers for Numpy and Pandas data.
 
-MLpype base pipelines/pipes should already be compatible with Sklearn, so you can use them directly.
+MLpype base pipelines/pipes should already be compatible with Sklearn transformers, so you can use them directly.
+StandardScaler for instance will work with MLpype.
 """
 from . import data, model, pipeline
