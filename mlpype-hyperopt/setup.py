5c946b815a1d8b761f7f47ce20af429cc8d460ba
Jeroen.vandenHoven <jeroen.vandenhoven@eneco.com>
2025-02-25 12:13:48 +0100

    V 0.6.4

--- mlpype-hyperopt/setup.py
+++ mlpype-hyperopt/setup.py
@@ -4,7 +4,7 @@ from typing import List
 from setuptools import find_namespace_packages, setup
 
 if __name__ == "__main__":
-    version = "0.6.3"
+    version = "0.6.4"
     deps: List[str] = [f"mlpype-base=={version}", "hyperopt>=0.2.7"]
     strict_deps = [s.replace(">=", "==") for s in deps]
 
